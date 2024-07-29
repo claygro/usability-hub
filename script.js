c3a89b9eed62ee3ef4bf28600a65da476a522c51
claygro <dhamala80130@gmail.com>
2024-07-29 19:21:02 +0545

    new bug fixed

--- script.js
+++ script.js
@@ -1,20 +1,6 @@
-let d = 0;
-let a = 0;
-const hide = () => {
-  if (d === 0) {
-    document.querySelector(".menu-nav").style.visibility = "visible";
-    document.querySelector(".hide").style.visibility = "hidden";
-  } else {
-    document.querySelector(".menu-nav").style.visibility = "hidden";
-    document.querySelector(".hide").style.visibility = "visiblem";
-  }
-};
-const show = () => {
-  if (a === 0) {
-    document.querySelector(".menu-nav").style.visibility = "hidden";
-    document.querySelector(".hide").style.visibility = "visible";
-  } else {
-    document.querySelector(".menu-nav").style.visibility = "visible";
-    document.querySelector(".hide").style.visibility = "hidden";
-  }
-};
+function show() {
+  document.querySelector(".menu-nav").style.display = "flex";
+}
+function hide() {
+  document.querySelector(".menu-nav").style.display = "none";
+}
